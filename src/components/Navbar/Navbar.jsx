b6e2777698aeaa66033a372d9acd4960f6b94dd4
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-09-29 12:53:53 -0700

    sidebar design+hide

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,13 +13,13 @@ import {
 } from './NavbarElements'
 
 
-const Navbar = () => {
+const Navbar = ({ toggle }) => {
   return (
     <>
       <Nav>
         <NavbarContainer>
           <NavLogo to="/">VowSuite</NavLogo>
-          <MobileIcon>
+          <MobileIcon onClick={toggle}>
             <FaBars />
           </MobileIcon>
           <NavMenu>
